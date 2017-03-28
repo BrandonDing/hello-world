40c4908d9f956a9b4c48aa2ad5dac74bdb119813
Abner <shangjin.ding@sihuatech.com>
2017-03-28 18:51:22 +0800

    Initial commit

---
+++ src/cn/com/yeexun/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.com.yeexun.demo;
+
+/**
+ * Created by Abner on 2017/3/28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, idea.");
+    }
+}
