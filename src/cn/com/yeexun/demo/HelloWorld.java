708c3b2033dc1c58390bef31a8499848b6af45ab
Abner <shangjin.ding@sihuatech.com>
2017-03-28 18:56:05 +0800

    commit first

--- src/cn/com/yeexun/demo/HelloWorld.java
+++ src/cn/com/yeexun/demo/HelloWorld.java
@@ -6,5 +6,6 @@ package cn.com.yeexun.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello, idea.");
+        System.out.println("this is an adding commit.");
     }
 }
